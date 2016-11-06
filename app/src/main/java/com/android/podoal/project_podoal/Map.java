fd2d6df8a5a8635fd329284e7a088c5c0324424c
YunKyun Han <kyunbaragi@daum.net>
2016-11-06 15:45:01 +0900

    no message

---
+++ app/src/main/java/com/android/podoal/project_podoal/Map.java
@@ -0,0 +1,8 @@
+package com.android.podoal.project_podoal;
+
+/**
+ * Created by YunKyun on 2016-11-06.
+ */
+
+public class Map {
+}
---
+++ app/src/main/java/com/android/podoal/project_podoal/MapsActivity.java
@@ -0,0 +1,74 @@
+package com.android.podoal.project_podoal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        cameraSetup();
+
+
+    }
+
+    private void cameraSetup(){
+        Button camera_btn = (Button)findViewById(R.id.camera_button);
+
+        camera_btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng seoul = new LatLng(37.56, 126.97);
+        mMap.addMarker( new MarkerOptions().position(seoul).title( "Marker in Seoul" ) );
+        mMap.moveCamera( CameraUpdateFactory.newLatLng(seoul));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 12));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.555873, 127.049488))
+                .title("Hanyang Univ. IT/BT"));
+    }
+}
