5f59027a070c02e4fef7da87ff00ef163ba0ae9d
suchiwon <suchiwon901@naver.com>
2016-11-14 00:07:24 +0900

    2016-11-14 00:00 서치원 commit - 맵에서의 gps 자신 위치 추적 및 사진 촬영시의 처리의 뼈대 코드 작성 및 DB 연결 클래스 분리

---
+++ app/src/main/java/com/android/podoal/project_podoal/dataquery/SelectQueryGetter.java
@@ -0,0 +1,39 @@
+package com.android.podoal.project_podoal.dataquery;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by tj on 2016-11-13.
+ */
+
+public class SelectQueryGetter extends AsyncTask<String, Void, String> {
+
+    protected String doInBackground(String... params) {
+        String uri = params[0];
+
+        BufferedReader bufferedReader = null;
+
+        try {
+            URL url = new URL(uri);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            StringBuilder sb = new StringBuilder();
+
+            bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String json;
+            while((json = bufferedReader.readLine())!= null){
+                sb.append(json+"\n");
+            }
+
+            return sb.toString().trim();
+
+        }catch(Exception e){
+            return null;
+        }
+    }
+}
