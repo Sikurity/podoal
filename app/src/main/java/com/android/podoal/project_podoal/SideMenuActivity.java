6e007fa44f42cda7f0542bd1a00ba8da52e481a1
YunKyun Han <kyunbaragi@daum.net>
2016-11-28 02:32:27 +0900

    현재 위치 출력 기능 추가

--- app/src/main/java/com/android/podoal/project_podoal/SideMenuActivity.java
+++ app/src/main/java/com/android/podoal/project_podoal/SideMenuActivity.java
@@ -2,11 +2,14 @@ package com.android.podoal.project_podoal;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,10 +23,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.podoal.project_podoal.datamodel.MemberInfo;
@@ -45,8 +50,10 @@ public class SideMenuActivity extends AppCompatActivity implements NavigationVie
     private List<VisitedSightDTO> visitedSightList;
     private Location location;
     private SelectQueryGetter dbSelector;
+    private boolean flag = false;
 
     static final int REQUEST_CAMERA = 1;
+    private TextView currentPosition = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -77,12 +84,15 @@ public class SideMenuActivity extends AppCompatActivity implements NavigationVie
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        cameraSetup();
-
         if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
             int locPermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
             if (locPermissionCheck != PackageManager.PERMISSION_GRANTED) {
-                getPermission(Manifest.permission.ACCESS_FINE_LOCATION, "위치");
+                getPermission(Manifest.permission.ACCESS_FINE_LOCATION, "위치_By_GPS");
+            }
+
+            int locPermissionCheck2 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+            if (locPermissionCheck2 != PackageManager.PERMISSION_GRANTED) {
+                getPermission(Manifest.permission.ACCESS_COARSE_LOCATION, "위치_By_Network");
             }
 
             int filePermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
@@ -91,8 +101,73 @@ public class SideMenuActivity extends AppCompatActivity implements NavigationVie
             }
         }
 
+        cameraSetup();
+
         System.out.println("SIDE_MENU_ACTIVITY_ON_CREATE_END");
     }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        currentPosition = (TextView) findViewById(R.id.current_position);
+        currentPosition.setText("Current Position");
+
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                    1, // 통지사이의 최소 변경거리 (m)
+                    mLocationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                    1, // 통지사이의 최소 변경거리 (m)
+                    mLocationListener);
+
+            flag = true;
+        }else{
+            Toast.makeText(this, "권한이 없습니다.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            //여기서 위치값이 갱신되면 이벤트가 발생한다.
+            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
+
+            Log.d("test", "onLocationChanged, location:" + location);
+            double longitude = location.getLongitude(); //경도
+            double latitude = location.getLatitude();   //위도
+            float accuracy = location.getAccuracy();    //정확도
+            //Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
+            //Network 위치제공자에 의한 위치변화
+            //Network 위치는 Gps에 비해 정확도가 많이 떨어진다.
+
+            //if(flag)
+            //{
+                currentPosition.setText("위도 : " + longitude
+                        + "\n경도 : " + latitude
+                        + "\n정확도 : "  + accuracy);
+            //}
+        }
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+    };
 
     private void getPermission(final String permissionName, String permissionNickname){
 
